368450130e5894b8773ed29980396abcefdb5834
shakibul islam <iamshakibulislam@gmail.com>
2024-01-02 20:11:29 +0600

    finished guest post collecting functions

--- backlinker.py
+++ backlinker.py
@@ -5,12 +5,14 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import QMovie, QPixmap,QDesktopServices
 import requests
 import re
+import time
 from functions import generate_headers
 import requests
 from bs4 import BeautifulSoup
 from fake_headers import Headers
 from email_scraper import scrape_emails
 import tldextract
+import csv
 
 class UI(QMainWindow):
 	def __init__(self):
@@ -25,12 +27,106 @@ class UI(QMainWindow):
 		self.mass_email_window_opener_btn = self.findChild(QPushButton,"send_mass_email_open_btn")
 		self.mass_email_window_opener_btn.clicked.connect(self.open_mass_email_window)
 		self.add_email_window_opener_btn = self.findChild(QPushButton,"add_email_account_window_opener_btn")
-		self.error = uic.loadUi('ui/error.ui')
 
-		self.set_table_headers(self.table,["Domain", "Email", "DA", "PA", "Spam Score"])
+		self.add_email_window_opener_btn.clicked.connect(self.open_email_form)
+		self.error = uic.loadUi('ui/error.ui')
+		self.savedbtn = uic.loadUi('ui/saved.ui')
+		self.email_form = uic.loadUi('ui/email_input_form.ui')
+
+		self.set_table_headers(self.table,["Domain", "Email", "DA", "PA", "Backlinks"])
+
+		self.loading_label = self.findChild(QLabel,"loading_label")
+
+		self.loading_bar = QMovie("static/progress_bar_loading.gif")
+		self.loading_label.setMovie(self.loading_bar)
+		self.loading_bar.start()
+
+		self.loading_label.hide()
+
+		self.download_label = self.findChild(QLabel,"download_img")
 		
 
+		
+
+		self.download_btn = QMovie("static/download_img.gif")
+		self.download_label.setMovie(self.download_btn)
+		self.download_btn.start()
+
+		self.download_label.installEventFilter(self)
+		self.download_label.hide()
+
+		#self.download_btn.clicked.connect(self.download)
+
+		#self.loading_label.hide()
+
+	#save button actions here.........
+	def eventFilter(self, obj, event):
+		if obj == self.download_label and event.type() == 2:
+			#open save dialouge and save the csv file
+			
+			self.download()
+            # Implement your desired action here
+			return True
+		return super().eventFilter(obj, event)
 	
+	def open_email_form(self):
+		self.email_form.show()
+	
+	def save_table_to_csv(self,table_widget, file_path):
+		try:
+			with open(file_path, 'w', newline='') as csvfile:
+				csv_writer = csv.writer(csvfile)
+				
+				# Write headers (column names)
+				headers = []
+				for column in range(table_widget.columnCount()):
+					headers.append(table_widget.horizontalHeaderItem(column).text())
+				csv_writer.writerow(headers)
+				
+				# Write data
+				for row in range(table_widget.rowCount()):
+					row_data = []
+					for column in range(table_widget.columnCount()):
+						item = table_widget.item(row, column)
+						if item is not None:
+							row_data.append(item.text())
+						else:
+							row_data.append('')
+					csv_writer.writerow(row_data)
+			
+			print(f"Table data saved successfully to {file_path}")
+			return True
+		except Exception as e:
+			print(f"Error saving table data: {e}")
+			return False
+
+
+
+	def download(self):
+		file_path=self.open_file_dialouge()
+		savefile = self.save_table_to_csv(self.table,file_path)
+
+		if savefile == True:
+			self.savedbtn.show()
+			self.download_label.hide()
+			print("hello saved")
+
+		else:
+			print("error saving file")
+
+
+	def open_file_dialouge(self):
+		options = QFileDialog.Options()
+        # Set the file dialog to save mode
+		file_name, _ = QFileDialog.getSaveFileName(self, "Save File", "", "Csv Files (*.csv);;All Files (*)", options=options)
+		if file_name:
+            # Do something with the selected file path (e.g., save a file)
+			print(f"Selected file path: {file_name}")
+            # Implement your file saving logic here
+
+			return file_name
+		else:
+			return None
 	
 	def open_mass_email_window(self):
 		self.mass_email_window.open()
@@ -62,10 +158,11 @@ class UI(QMainWindow):
 		
 
 		
-
+		self.loading_label.show()
 		self.thread = Worker(keyword,site_amount)
 		self.thread.finished_signal.connect(self.proccess_done)
 		self.thread.update_signal.connect(self.update_status)
+		self.find_guest_post.hide()
 		self.thread.start()
 		
 
@@ -78,7 +175,9 @@ class UI(QMainWindow):
 
 	def proccess_done(self):
 		print("done")
-		pass
+		self.find_guest_post.show()
+		self.loading_label.hide()
+		self.download_label.show()
 
 		
 	
@@ -88,7 +187,7 @@ class UI(QMainWindow):
 		table.setColumnCount(len(headers))
 		table.setHorizontalHeaderLabels(headers)
 		total_width = table.viewport().size().width()
-		for col, percentage in enumerate([35,40,5,5,15]):
+		for col, percentage in enumerate([35,35,5,5,15]):
 			size = int(total_width * (percentage / 100))
 			table.setColumnWidth(col, size)
 
@@ -178,9 +277,15 @@ class Worker(QThread):
 		url_list = []
 		
 		for pagenum in range(total_pages):
-			#self.update_signal.emit({"domain":"https://facebook.com","email":"iamshakibulislam@gmail.com","DA":0,"PA":0,"Spam_score":10})
+			self.update_signal.emit({"domain":"https://facebook.com","email":"iamshakibulislam@gmail.com","DA":0,"PA":0,"Spam_score":10})
 			#self.finished_signal.emit(1)
 			print("loop starting - ",pagenum)
+
+			time.sleep(10)
+
+			self.finished_signal.emit(1)
+
+			break
 			
 
 			if pos_tracking == 0:
@@ -216,6 +321,8 @@ class Worker(QThread):
 
 			#next update the table adding these new rows on the GUI table . emit the update
 			print("done task...")
+
+			self.finished_signal.emit(1)
 			break
 
 		
